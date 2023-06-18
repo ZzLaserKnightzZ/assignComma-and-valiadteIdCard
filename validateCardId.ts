7fe3903390bad926457994f91bc3cee8cc61f562
ZzLaserKnightzZ <a.anu_pong-5759@hotmail.com>
2023-06-18 22:12:57 +0700

    Update validateCardId.ts

--- validateCardId.ts
+++ validateCardId.ts
@@ -1,13 +1,12 @@
-  const validateCardId = (id: string) => {
+    const validateCardId = (id: string) => {
 
-    const degit = id.split("");
-    let sum = 0;
-    let index = 0;
-
-    for (let i = 13; i >= 2; i--) {
-      sum += Number(degit[index]) * i;
-      index++;
+        if (id.length !== 13) return false;
+        const degit = id.split("");
+        let sum = 0;
+        let index = 0;
+        for (let i = 13; i >= 2; i--) {
+            sum += Number(degit[index]) * i;
+            index++;
+        }
+        return 11 - (sum % 11) === Number(degit[12]);
     }
-
-    return 11 - (sum % 11) === Number(degit[12]);
-  }
