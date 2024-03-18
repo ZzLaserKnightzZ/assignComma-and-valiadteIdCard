ff187ab898e005a1adabc2b1e4e4819ecdfc0803
ZzLaserKnightzZ <a.anu_pong-5759@hotmail.com>
2024-03-18 14:30:37 +0700

    Update assignComma.ts

--- assignComma.ts
+++ assignComma.ts
@@ -1,9 +1,10 @@
 export const assignComma = (num: number | null | undefined) => {
-  if (num) {
-    const regx = new RegExp("[0-9]{1,3}", "gi");
-    let numStr = (num + "").split("").reverse().join("");
-    let retStr = numStr.match(regx)?.join(",").split("").reverse().join("");
-    return retStr;
-  }
-  return "";
+            if (num) {
+                const regx = new RegExp("[0-9]{1,3}", "gi");
+                const float_num = num.split(".");
+                let numStr = (float_num[0] + "").split("").reverse().join("");
+                let retStr = numStr.match(regx)?.join(",").split("").reverse().join("");
+                return float_num.length > 1 ? retStr +"."+ float_num[1] : retStr;
+            }
+            return "";
 }
